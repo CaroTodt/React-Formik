d6d1d1469b14a956bb02b5847eb8377dc6c7e32e
Carolina Todt <carolina.todt@rosariosoftware.com>
2021-09-13 10:16:45 -0300

    add more components of the course

--- demo-formik-react/src/App.js
+++ demo-formik-react/src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 // import YoutubeForm from './components/YoutubeForm';
 // import FormikContainer from './components/FormikContainer';
 import LoginForm from './components/LoginForm';
+// import RegistrationForm from './components/RegistrationForm';
+// import EnrollmentForm from './components/EnrollmentForm';
+import { Theme, ThemeProvider } from "@chakra-ui/react"
 
 function App() {
   return (
+    <ThemeProvider theme={Theme}>
     <div className="App">
+      {/* <EnrollmentForm/> */}
       <LoginForm/>
+      {/* <RegistrationForm/> */}
       {/* <FormikContainer/> */}
      {/* <YoutubeForm/> */}
     </div>
+    </ThemeProvider>
   );
 }
 
